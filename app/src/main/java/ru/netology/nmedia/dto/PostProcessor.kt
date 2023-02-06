c0f9434077d97548d9eb0c56aee18fec753d1637
Starikov Artem <bobistrue@gmail.com>
2023-02-07 00:04:46 +0500

    исправил IF на WHEN в классе PostProcessor

--- app/src/main/java/ru/netology/nmedia/dto/PostProcessor.kt
+++ app/src/main/java/ru/netology/nmedia/dto/PostProcessor.kt
@@ -1,11 +1,11 @@
 package ru.netology.nmedia.dto
 
 object PostProcessor {
-    fun processQuantity(qty: Int): String {
-        if (qty < 1_000) return qty.toString()
-        else if (qty < 10_000) return (qty / 1_000).toString() + "." + (qty % 1000 / 100).toString() + "K"
-        else if (qty < 100_000) return (qty / 1_000).toString() + "K"
-        else if (qty < 1_000_000) return (qty / 1_000_000).toString() + "." + (qty % 1_000_000 / 100_000).toString() + "M"
-        else return (qty / 1_000_000).toString() + "M"
+    fun processQuantity(qty: Int) = when {
+        qty < 1_000 -> qty.toString()
+        qty < 10_000 -> (qty / 1_000).toString() + "." + (qty % 1000 / 100).toString() + "K"
+        qty < 100_000 -> (qty / 1_000).toString() + "K"
+        qty < 1_000_000 ->  (qty / 1_000_000).toString() + "." + (qty % 1_000_000 / 100_000).toString() + "M"
+        else -> (qty / 1_000_000).toString() + "M"
     }
 }
